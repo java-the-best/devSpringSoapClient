1eec6942ca12e98d1d8c6024cca48f2d5a65a751
Maxim Viktorov <victorov_maxim@mail.ru>
2022-04-21 08:34:45 +0300

    springBoot

---
+++ src/main/java/localhost/devSpringSoapClient/DevSpringSoapClientApplication.java
@@ -0,0 +1,13 @@
+package localhost.devSpringSoapClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevSpringSoapClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevSpringSoapClientApplication.class, args);
+	}
+
+}
